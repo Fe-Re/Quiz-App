c5d584a75b4edeadc34f98ac3697dbe759e1b38c
Fe-Re <Felixreisenhauer@googlemail.com>
2025-02-27 17:08:39 +0100

    Add category selection functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import { QuestionBox } from "./QuestionBox";
+import PropTypes from "prop-types";
 
 export default function App() {
   const [questionAnswer, setQuestionAnswer] = useState([]);
+  const [category, setCategory] = useState("");
 
   useEffect(() => {
-    fetch("https://the-trivia-api.com/v2/questions")
+    fetch(
+      `https://the-trivia-api.com/v2/questions${
+        category === "" ? "" : `?category=${category}`
+      }`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log("Fetched data:", data);
@@ -17,6 +23,28 @@ export default function App() {
   return (
     <>
       <QuestionBox questionAnswer={questionAnswer} />
+      <CategoriesSelector onSetCategory={setCategory} />
     </>
+  );
+}
+
+function CategoriesSelector({ onSetCategory }) {
+  function changeCategory(selection) {
+    onSetCategory(selection);
+  }
+
+  CategoriesSelector.propTypes = {
+    onSetCategory: PropTypes.func.isRequired,
+  };
+
+  return (
+    <select onChange={(event) => changeCategory(event.target.value)}>
+      <option value="">Select a category</option>
+      <option value="science">Science</option>
+      <option value="history">History</option>
+      <option value="sports_and_leisure">Sports and leisure</option>
+      <option value="music">Music</option>
+      <option value="geography">Geography</option>
+    </select>
   );
 }
